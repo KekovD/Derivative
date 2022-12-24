caf54b87912d1701e938efd9b3ad6cc78889bef5
KekovD <112900044+KekovD@users.noreply.github.com>
2022-12-24 05:14:07 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,6 +1,7 @@
 #ifndef DERIVATIVE_PARSER_H
 #define DERIVATIVE_PARSER_H
 #include <iostream>
+#include <string>
 
 enum functions
 {
@@ -19,16 +20,16 @@ public:
 
     tree_element ();
 
-    void processing (char*);  // преобразование выражения
-    tree_element *build (char*, int it1 = 0, int it2 = 0, int complex_oper = 0); // дерево для исходного выражения
-    tree_element *multiplication_and_division (char*, int, int); // умножение и деление элементов
-    tree_element *create_leaf (char*, int, int); // создание лепестка дерева
+    void processing (std::string);  // преобразование выражения
+    tree_element *build (std::string, int = 0, int = 0, int = 0); // дерево для исходного выражения
+    tree_element *multiplication_and_division (std::string, int, int); // умножение и деление элементов
+    tree_element *create_leaf (std::string, int, int); // создание лепестка дерева
     static bool is_digit (tree_element*); // проверка на число
     static bool is_zero (tree_element*); // проверка на нулевой элемент
     static bool is_one (tree_element*); // является число 1
     void ch_op (tree_element*); // инверсия оператора в элементе
     void change_operation (tree_element*); // замена оператора в дереве
-    void calculate (tree_element*, char oper = '+'); // вычисление значения в лепестке
+    void calculate (tree_element*, char = '+'); // вычисление значения в лепестке
     void simplify (tree_element*); // упрощение элемента
     void differentiation (tree_element*, char); // дифференцирование элемента по ключу переменной
     void LRR (tree_element*, std::ostream&); // обход дерева
@@ -49,15 +50,15 @@ public:
 
     void add (char);  // добавляет символ в стек
     void clear () ;  // очистка стека
-    char* get_string (char* s = nullptr);  // возвращает составленную строку
+    char* get_string (char* = nullptr);  // возвращает составленную строку
 };
 
-char* get_token (char*, int&); // Берет из строки токен с номером ind
+char* get_token (std::string, int&); // Берет из строки токен с номером ind
 functions is_function (char*);  // Проверяет, является ли строка функцией
 char* get_function_name (functions);  // Возвращает имя функции по ее индексу
-int analysis (char*, int&);  // Производит анализ правильности ввода
+int analysis (std::string, int&);  // Производит анализ правильности ввода
 int char_to_int (char);  // Преобразует символ в число соответствующее символу
-float get_number (char*, int&);  // Берет токен-число со строки
-void ignore_brackets (char*, int &);  // Игнорирование выражение в скобках
+float get_number (std::string, int&);  // Берет токен-число со строки
+void ignore_brackets (std::string, int&);  // Игнорирование выражение в скобках
 
 #endif //DERIVATIVE_PARSER_H
